0b5bbe6d6de18f5d5ef064d24cb477fe5506b563
zinosama <rhu5@u.rochester.edu>
2019-11-25 14:15:35 -0700

    Remove duplicate to_s

--- lib/omniauth/auth0/jwt_validator.rb
+++ lib/omniauth/auth0/jwt_validator.rb
@@ -127,8 +127,8 @@ module OmniAuth
       def uri_string(uri)
         temp_domain = URI(uri)
         temp_domain = URI("https://#{uri}") unless temp_domain.scheme
-        temp_domain = "#{temp_domain}/" unless temp_domain.to_s.end_with?('/')
-        temp_domain.to_s
+        temp_domain = temp_domain.to_s
+        temp_domain.end_with?('/') ? temp_domain : "#{temp_domain}/"
       end
     end
   end
