ab69ff4fc9b615ea473f84e0a9051af8d6158097
Ryan Ebue <generyan.ebue@gmail.com>
2021-09-30 19:17:33 +1300

    more logging

--- lib/omniauth/strategies/auth0.rb
+++ lib/omniauth/strategies/auth0.rb
@@ -140,9 +140,10 @@ module OmniAuth
         error = request.params["error_reason"] || request.params["error"]
         Rails.logger.info "Auth0 Error: #{error}"
         Rails.logger.info "Auth0 Request: #{request.params}"
-        Rails.logger.info "\n\n\n\n\n Auth0 HASH: #{auth_hash}"
+        # Rails.logger.info "\n\n\n\n\n Auth0 HASH: #{auth_hash}"
         # env['omniauth.auth'] = auth_hash
         # call_app!
+        Rails.logger.info "Auth0 before super call"
         super
       rescue OmniAuth::Auth0::TokenValidationError => e
         fail!(:token_validation_error, e)
